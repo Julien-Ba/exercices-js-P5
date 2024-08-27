051eff1014d3b68770c8af88f844ef9422afa83e
julien-ba <julienbarbier42@gmail.com>
2024-08-27 16:45:43 +0200

    Exercice9

--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,3 +1,7 @@
-// votre code ici
-
 export default checkAge;
+
+function checkAge(a) {
+    if (a < 18) return "Vous êtes mineur.";
+    if (a >= 18 && a <= 65) return "Vous êtes majeur.";
+    return "Vous êtes senior.";
+}
